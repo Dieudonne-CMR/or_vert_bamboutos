1dd297e9ba1e3cb6a44aec9e018c18138af5112b
@HTRJ1972 <joelhapi6@gmail.com>
2024-11-14 09:41:29 +0100

    ce que j'ai fait aujourd'hui : traduction des textes du francais vers l'anglais

--- template/service.php
+++ template/service.php
@@ -66,7 +66,7 @@
             </div>
             <div class="breadcumb-menu-wrap">
                 <ul class="breadcumb-menu">
-                    <li><a href="home.php">Accueil</a></li>
+                    <li><a href="home">Accueil</a></li>
                     <li>Nos Services</li>
                 </ul>
             </div>
