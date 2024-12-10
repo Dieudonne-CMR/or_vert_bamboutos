d33ad0833223fbd8390dadef862cb748f3c949a4
Dieudonne-CMR <dieudonne.bayiha@goldenrock.io>
2024-12-10 18:57:04 +0100

    ajout la matricule de or_vert

--- api_shops/cle_api_shop.php
+++ api_shops/cle_api_shop.php
@@ -1,9 +1,10 @@
 
 <?php 
-//----Lien des images de produits
+//----Lien des images de produits 
 $image_produit = "https://akila.store/_ressources/images_articles/";
 //-----
-$id_website= '7220708820';
+$id_website= '5062126872';
+// $id_website= '7220708820';
 
 include "api_shops/api_produit.php";
 include "api_shops/api_boutique.php";
--- includes/fonctions/fonctions.php
+++ includes/fonctions/fonctions.php
@@ -1,6 +1,8 @@
 <?php
 function court_text($text,$nombre_caracteres){
+    $text= strip_tags($text);
     if(strlen($text)>22){ 
+        
         $text = substr($text,0,$nombre_caracteres)." ...";
     } 
     return $text;
